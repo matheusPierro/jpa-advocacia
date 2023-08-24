4cc2ab3e5ea9b22b8030cd1bd77ab21fbcc754e3
Matheus Ramos de Pierro <matpierro570@gmail.com>
2023-08-23 22:58:17 -0300

    Melhoras de leitura

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -16,29 +16,27 @@ public class Main {
 
         TipoDeAcao tipoAcao = new TipoDeAcao();
         tipoAcao.setNome("Agressão");
-
         Estado estado = new Estado();
         estado.setNome("Minas Gerais").setSigla("MG");
-
         Advogado advogado = new Advogado();
         advogado.setNome("Victor Shimada").setNumeroOAB("V15").setEstado(estado);
-
         Processo processo = new Processo();
         processo.setNumero("1563").setProBono(true).setTipoDeAcao(tipoAcao).setAdvogado(advogado);
 
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("oracle");
         EntityManager manager = factory.createEntityManager();
 
-        List<Processo> processos = manager.createQuery("FROM Processo ").getResultList();
-        Processo processoSelecionado = (Processo) JOptionPane.showInputDialog(null, "Selecione um processo", "Seleção de processos", JOptionPane.QUESTION_MESSAGE, null, processos.toArray(), processos.get(0));
-        Processo processoById = manager.find(Processo.class, 1L);
+//        List<Processo> processos = manager.createQuery("FROM Processo ").getResultList();
+//        Processo processoSelecionado = (Processo) JOptionPane.showInputDialog(null, "Selecione um processo", "Seleção de processos", JOptionPane.QUESTION_MESSAGE, null, processos.toArray(), processos.get(0));
+//        Processo processoById = manager.find(Processo.class, 1L);
 
         manager.getTransaction().begin();
         manager.persist(processo);
         manager.getTransaction().commit();
+
         System.out.println(processo);
-        System.out.println(processoSelecionado);
-        System.out.println(processoById);
+//        System.out.println(processoSelecionado);
+//        System.out.println(processoById);
 
         manager.close();
         factory.close();
